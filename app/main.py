109499c2cf26438706c1812cd135063e7741ae08
oscargh945 <oscargarciaherrera49@gmail.com>
2025-05-28 18:55:51 -0500

    Minor fix

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Configura CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],  # agrega aquí los dominios de tu frontend
+    allow_origins=["*"],  # agrega aquí los dominios de tu frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
